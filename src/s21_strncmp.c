f8ee77fba10a808f5edb4937b3afbae485b2a3dc
Addison Action <addisona@ev-a5.kzn.21-school.ru>
2022-04-28 16:10:15 +0300

    lo

--- src/s21_strncmp.c
+++ src/s21_strncmp.c
@@ -1,10 +1,17 @@
 #include "s21_string.h"
 
 int s21_strncmp(const char *str1, const char *str2, s21_size_t n) {
-    for (s21_size_t i = 0; i < n; i++) {
-        if (str1[i] != str2[i]) {
-            return str1[i] - str2[i];
-        }
+    
+    s21_size_t i;
+    
+    i = 0;
+    while ((str1[i] || str2[i]) && (i < n))
+    {
+        if (str1[i] < str2[i])
+            return -1;
+        if (str1[i] > str2[i])
+            return 1;
+        i++;
     }
     return 0;
 }
