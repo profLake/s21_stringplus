415bf00cc3ffa7db1549edcdecde594c6415ea1d
Addison Action <addisona@ev-b2.kzn.21-school.ru>
2022-05-01 18:06:14 +0300

    pythonStyle

--- src/s21_strncmp.c
+++ src/s21_strncmp.c
@@ -1,13 +1,10 @@
 #include "s21_string.h"
 
 int s21_strncmp(const char *str1, const char *str2, s21_size_t n) {
-    
     s21_size_t i;
     int ret = 0;
-    
     i = 0;
-    while ((str1[i] || str2[i]) && (i < n))
-    {
+    while ((str1[i] || str2[i]) && (i < n)) {
         if (str1[i] != str2[i]) {
             ret = str1[i] - str2[i];
             break;
