81519fca85ddd6bbe99fba865a50f9732222f27f
Ganymede Legate <ganymedl@ev-b1.kzn.21-school.ru>
2022-05-02 18:22:19 +0300

    gany

--- src/s21_to_upper.c
+++ src/s21_to_upper.c
@@ -8,7 +8,7 @@ void *s21_to_upper(const char *str) {
     while(str[t] != 0) {
         t++;
     }
-    t++;
+    printf("%d\n", t);
     char *srr = (char*)malloc(t * sizeof(char));
     for(int q = 0; str[q] != 0; q++) {
         srr[q] = str[q];
@@ -17,16 +17,4 @@ void *s21_to_upper(const char *str) {
         }
     }
     return srr;
-    free(srr);
 }
-
-/*
-int main() {
-    char str[20];
-    scanf("%s", str);
-    char *src = s21_to_upper(str);
-    printf("%s\n", src);
-    printf("%s", str);
-    return 0;
-}
-*/
