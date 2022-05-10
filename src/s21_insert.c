9ae275914c9f973a82fc6f4f6360591057e0ac94
Vickey Clockwork <vickeycl@ev-b4.kzn.21-school.ru>
2022-05-10 20:16:31 +0300

    new

--- src/s21_insert.c
+++ src/s21_insert.c
@@ -8,6 +8,7 @@ void *s21_insert(const char *src, const char *str, s21_size_t start_index) {
   s21_size_t q = 0;
   s21_size_t r;
   s21_size_t k = 0;
+  char *m = s21_NULL;
 
   while (src[i] != '\0') {
     i++;
@@ -17,36 +18,36 @@ void *s21_insert(const char *src, const char *str, s21_size_t start_index) {
     q++;
   }
 
-  char *m = malloc((i + q - 1) * sizeof(char));
+  if (i >= start_index) {
+    m = malloc((i + q + 1) * sizeof(char));
 
-  for (r = 0; src[r - k] != '\0'; r++) {
-    if (r < start_index) {
-      m[r] = src[r];
+    for (r = 0; r < i + q + 1; r++) {
+      if (r < start_index) {
+        m[r] = src[r];
 
-    } else if (r >= start_index && str[k] != '\0') {
-      m[r] = str[k];
-      k++;
+      } else if (r >= start_index && str[k] != '\0') {
+        m[r] = str[k];
+        k++;
+      }
+      if (r >= (start_index + q) && src[r - k - 1] != '\0') {
+        m[r] = src[r - k];
+      }
     }
-    if (r >= (start_index + q) && src[r - k - 1] != '\0') m[r] = src[r - k];
   }
-  if (start_index > i) {
-    m = s21_NULL;
-  }
+
   return m;
 }
-
 /*
 int main() {
+  char *q = "geronimo";
 
+  char *k = "bazinga";
 
+  char *z = s21_insert(q, k, 0);
 
-    char * q = "geronimo";
+  printf("%s", z);
 
-    char * k = "bazinga";
-
-    char * z = s21_insert(q, k, 5);
-
-    printf("%s", z);
-
+  free(z);
+  z = NULL;
 }
 */
