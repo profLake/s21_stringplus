2e562f9f7eebd842475ec0a8f7385c710b251f13
artur <muslimow9@yandex.ru>
2022-05-16 19:35:16 +0300

    Added sce_vick_Makefile_ to suit Linux. Fixed s21_memtok.c
     On branch develop
     Changes to be committed:
    	modified:   s21_memcmp.c
    	modified:   s21_strtok.c
    	new file:   sce_vick_Makefile_
    	modified:   vick_Makefile_

--- src/s21_memcmp.c
+++ src/s21_memcmp.c
@@ -7,6 +7,7 @@ int s21_memcmp(const void *str1, const void *str2, s21_size_t n) {
     for (s21_size_t i = 0; i < n; i++) {
         if (p1[i] != p2[i]) {
             ret = p1[i] - p2[i];
+            //ret = p1[i] < p2[i] ? -1 : 1;
             break;
         }
     }
--- src/s21_strtok.c
+++ src/s21_strtok.c
@@ -11,7 +11,7 @@ char *s21_strtok(char *str, const char *delim) {
         return s21_NULL;
     }
 
-    /* Пропускаем неподходящую часть str */
+    /* Пропускаем неподходящую часть str, состоящую из символов-разделителей */
     while (*str && s21_strpbrk(str, delim) == str) {
         str++;
     }
@@ -19,14 +19,18 @@ char *s21_strtok(char *str, const char *delim) {
         return s21_NULL;
     }
 
-    /* Ищем конец токена. Если находим, то ставим '\0' на его
-     * месте. Также сдвигаем str_saved на этот конец для будущих
-     * вызовов
+    /*  Ищем конец токена. Если находим, то ставим '\0' на его
+     *      месте. Также сдвигаем str_saved на этот конец для будущих
+     *      вызовов.
+     *  Если мы дошли до конца str, но не встретили разделителя, обнуляем
+     *      str_saved, возвращая функцию в исходное состояние.
      */
     char *token_end = s21_strpbrk(str, delim);
     if (token_end) {
         *token_end = '\0';
         str_saved = token_end + 1;
+    } else {
+        str_saved = s21_NULL;
     }
 
     return str;
