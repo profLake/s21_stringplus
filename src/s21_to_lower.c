0b66913b75c61de6a3f46f83781ea7df3bc56cf3
Sato Porridge <satoporr@ev-b6.kzn.21-school.ru>
2022-05-18 10:28:32 +0300

    CPPLINT_new

--- src/s21_to_lower.c
+++ src/s21_to_lower.c
@@ -1,28 +1,25 @@
 #include <stdlib.h>
+
 #include "s21_string.h"
 
 void *s21_to_lower(const char *str) {
-    int i;
-    i = 0;
-    int j;
-    j = 0;
-    
-    while (str[i] != '\0') {
-        i++;
+  int i;
+  i = 0;
+  int j;
+  j = 0;
+  while (str[i] != '\0') {
+    i++;
+  }
+  char *scopy = malloc(i * sizeof(char));
+  int q;
+  for (q = 0; str[q] != '\0'; q++) {
+    scopy[q] = str[q];
+  }
+  while (scopy[j]) {
+    if (scopy[j] >= 65 && str[j] <= 90) {
+      scopy[j] = scopy[j] + 32;
     }
-
-    char *scopy = malloc(i * sizeof(char));
-    
-    int q;
-    for (q=0; str[q] != '\0'; q++) {
-        scopy[q] = str[q];
-    }
-    while (scopy[j]) {
-        if (scopy[j] >= 65 && str[j] <= 90) {
-            scopy[j] = scopy[j] + 32;
-        }
-        j++;
-    }
-    return scopy;
+    j++;
+  }
+  return scopy;
 }
-
